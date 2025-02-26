b81edf0a6fe7dc5ce44e541dd00bec5cac7d5f35
Ricardo Monterrsoa <48804047+ricky-antonio@users.noreply.github.com>
2025-02-26 10:30:00 -0800

    UI setup

--- main.py
+++ main.py
@@ -16,21 +16,37 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+#window setup
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+
+#Canvas setup
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="./tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
 
+fg=GREEN
+check_mark = "✓"
 
+#Timer label
+timer_label = Label(text="Timer", font=(FONT_NAME, 35, "bold"), bg=YELLOW, fg=GREEN)
+timer_label.grid(column=1, row=0)
 
+#check label
+check_label = Label(text=check_mark, font=(FONT_NAME, 28, "bold"),bg=YELLOW, fg=GREEN)
+check_label.grid(column=1, row=3)
 
+#Start Button
+start_button = Button(text="Start")
+start_button.grid(column=0, row=2)
 
-
+#Reset Button
+reset_button = Button(text="Reset")
+reset_button.grid(column=2, row=2)
 
 
 window.mainloop()
